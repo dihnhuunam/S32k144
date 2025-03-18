89dcb1ce9abe4e2a71a88a34cd2879b56fa16d64
dihnhuunam <dihnhuunam@gmail.com>
2025-03-18 13:56:55 +0700

    [ADD]: Interrupt My NVIC

--- Interrupt/src/main.c
+++ Interrupt/src/main.c
@@ -10,6 +10,12 @@ void InitSW(uint8_t swPin);
 void ToggleLed(uint8_t ledPin);
 void InitInterrupt(uint8_t swPin);
 
+void MyNVIC_EnableIRQ(uint8_t IRQn);
+void MyNVIC_DisableIRQ(uint8_t IRQn);
+void MyNVIC_SetPendingIRQ(uint8_t IRQn);
+void MyNVIC_ClearPendingIRQ(uint8_t IRQn);
+void MyNVIC_SetPriority(uint8_t IRQn, uint8_t priority);
+
 volatile uint8_t g_ledPin = LED_GREEN;
 volatile uint8_t g_swPin = SW2;
 
@@ -49,9 +55,10 @@ void InitSW(uint8_t swPin)
 	// 1. Enable clock for PORTC
 	PCC->PCCn[PCC_PORTC_INDEX] |= PCC_PCCn_CGC(1);
 
-	// 2. Set MUX as GPIO, enable Passive Filter, disable  Pull-up
-	PORTC->PCR[swPin] |= PORT_PCR_MUX(1) | PORT_PCR_PFE(1);
-	PORTC->PCR[swPin] &= ~PORT_PCR_PE(1);
+	// 2. Set MUX as GPIO, enable Passive Filter, disable  Pull-up, enable interrupt on Falling Edge
+	PORTC->PCR[swPin] |= PORT_PCR_MUX(1) | PORT_PCR_PFE(1); // Set MUX as GPIO
+	PORTC->PCR[swPin] &= ~PORT_PCR_PE(1);					// Enable Passive Filter
+	PORTC->PCR[swPin] |= PORT_PCR_IRQC(0xA);				// Enable interrupt on Falling Edge
 
 	// 3. Set data direction as input
 	PTC->PDDR &= ~(1 << swPin);
@@ -64,11 +71,17 @@ void ToggleLed(uint8_t ledPin)
 
 void InitInterrupt(uint8_t swPin)
 {
-	// Enable interrupt on Falling Edge
-	PORTC->PCR[swPin] |= PORT_PCR_IRQC(0xA);
+	// 1. Clear any pending IRQ
+	INT_SYS_ClearPending(PORTC_IRQn);
+	// MyNVIC_ClearPendingIRQ(PORTC_IRQn);
 
-	// Enable NVIC Interrupt
+	// 2. Set priority
+	INT_SYS_SetPriority(PORTC_IRQn, 201);
+	// MyNVIC_SetPendingIRQ(PORTC_IRQn, 201);
+
+	// 3. Enable NVIC Interrupt
 	INT_SYS_EnableIRQ(PORTC_IRQn);
+	// MyNVIC_EnableIRQ(PORTC_IRQn);
 }
 
 // Interrupt Service Routine (ISR)
@@ -79,4 +92,29 @@ void PORTC_IRQHandler(void)
 		ToggleLed(g_ledPin);
 		PORTC->ISFR |= (1 << g_swPin);
 	}
+}
+
+void MyNVIC_EnableIRQ(uint8_t IRQn)
+{
+	S32_NVIC->ISER[IRQn >> 5] |= (1 << (IRQn % 32));
+}
+
+void MyNVIC_DisableIRQ(uint8_t IRQn)
+{
+	S32_NVIC->ICER[IRQn >> 5] |= (1 << (IRQn % 32));
+}
+
+void MyNVIC_SetPendingIRQ(uint8_t IRQn)
+{
+	S32_NVIC->ISPR[IRQn >> 5] |= (1 << (IRQn % 32));
+}
+
+void MyNVIC_ClearPendingIRQ(uint8_t IRQn)
+{
+	S32_NVIC->ICPR[IRQn >> 5] |= (1 << (IRQn % 32));
+}
+
+void MyNVIC_SetPriority(uint8_t IRQn, uint8_t priority)
+{
+	S32_NVIC->IP[IRQn] = priority;
 }
